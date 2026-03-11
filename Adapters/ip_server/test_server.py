b69d6ae1f4f126af6ee891930f9b7eafa8df9eb7
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2026-03-11 20:06:33 +0100

    sync

--- Adapters/ip_server/test_server.py
+++ Adapters/ip_server/test_server.py
@@ -1,14 +1,26 @@
-from syndesi.adapters import IP, Remote
-from syndesi.tools.log import set_log_stream
+import time
+from syndesi import IP, IPServer, AdapterEvent
+
+def callback(client : IP, event : AdapterEvent):
+    """..."""
+    print(f'Callback {client} {event}')
 
 def main():
+    """..."""
+    server = IPServer('127.0.0.1', port=4242)
+    server.register_client_callback(callback)
+    client = IP('127.0.0.1', port=4242, auto_open=False)
 
-    #set_log_stream(True, 'DEBUG')
-    my_adapter = Remote(IP('pcsebastien'), IP('tcpbin.com', port=4242))
+    client_opened = client.try_open()
+    print(f'Client opened : {client_opened}')
+    
+    time.sleep(0.5)
+    print('Client write')
+    client.write(b'123\n')
 
-    output = my_adapter.query('Hello World\n')
 
-    print(output)
+    client.close()
+    server.close()
 
 
 if __name__ == '__main__':
